74ca7beef4f620dd0bd98d54c8442fee2a16df4a
Waiting7777 <waiting0819@msn.com>
2016-11-15 15:33:45 +0800

    Update 2016-11-15T07:33:11.507Z

--- src/js/charts/chart-config.js
+++
@@ -1,7 +0,0 @@
-// Object containing chart types and their respective configs
-var chartConfig = {
-	xy: require("./cb-xy/xy-config"),
-	chartgrid: require("./cb-chart-grid/chart-grid-config"),
-};
-
-module.exports = chartConfig;
--- test/jsx/index-jsx.js
+++
@@ -1,4 +0,0 @@
-require("./chartbuilder.jsx");
-require("./xy-renderer.jsx");
-require("./chart-grid-bar.jsx");
-require("./chart-grid-xy.jsx");
